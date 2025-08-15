e479ff0de0d18d3277117bb7ece2dd940fe44dc8
PHET45 <phnjk2000@gmail.com>
2025-08-16 01:42:24 +0700

    Enhance ArticleSection component by adding a heading for latest articles and updating input attributes for consistency in casing.

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Input } from "@/components/ui/input"
+import React from 'react';
+import { Input } from "@/components/ui/input";
 
 const ArticleSection = () => {
   return (
     <div>
+    <h3 className='font-Poppins font-semibold text-2xl leading-8'>Latest articles</h3>
+    
         <div className="grid w-full max-w-sm items-center gap-3">
-            <Input type="search" id="search" placeholder="search" />
+            <Input type="Search" id="Search" placeholder="Search" />
         </div>
+    
     </div>
   )
 }
