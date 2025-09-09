82564514d3be782dcfd9e458a99dd89f59f32c8b
PHET45 <phnjk2000@gmail.com>
2025-09-09 18:27:35 +0700

    Enhance Footer component with improved interactivity and styling
    - Added cursor pointer class to social media icons for better user experience.
    - Updated Home page button to include cursor pointer for improved interactivity.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ export const Footer = () => {
     <div className="flex flex-row justify-evenly items-center lg:justify-start lg:gap-10"> 
         <div className="font-poppins font-medium text-base leading-6 tracking-normal">Get in touch</div>
             <div className="flex flex-row gap-5">
-                <div><img src="/public/linkedin.svg" alt="linkedin"/></div>
-                <div><img src="/public/github.svg" alt="github"/></div>
-                <div><img src="/public/google.svg" alt="google"/></div>
+                <div><img src="/public/linkedin.svg" alt="linkedin" className="cursor-pointer"/></div>
+                <div><img src="/public/github.svg" alt="github" className="cursor-pointer"/></div>
+                <div><img src="/public/google.svg" alt="google" className="cursor-pointer"/></div>
             </div>
     </div>
-    <div className="flex items-center justify-center lg:justify-end lg:px-5 lg:my-[-30px] py-5 cursor-pointer underline"><button>Home page</button></div>
+    <div className="flex items-center justify-center lg:justify-end lg:px-5 lg:my-[-30px] py-5  underline"><button className="cursor-pointer">Home page</button></div>
 </div>
   )
 }
