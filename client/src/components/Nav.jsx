f4aae9b74002c7f4b770880f40fc94ba8d46a78f
PHET45 <phnjk2000@gmail.com>
2025-09-10 18:54:30 +0700

    feat: Update Navbar component to use routing with Link for Login and Signup buttons
    - Replaced button elements with Link components for Login and Signup in the Navbar, enhancing navigation consistency and user experience.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-  
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -7,10 +7,10 @@ const Navbar = () => {
         <img src="/public/images/logo.png" alt="logo" />
         <ul className="flex flex-row gap-5 ">
           <li>
-            <button className="text-black bg-white   hover:bg-white focus:outline-none  focus:ring-stone-400 font-medium rounded-full text-sm  me-2 mb-2 dark:bg-white dark:hover:border-stone-400 dark:hover:text-stone-400 dark:focus:ring-stone-400 dark:border-gray-800 px-[40px] py-[12px] border-1">Login</button>
+            <Link to="/login"><button className="text-black bg-white   hover:bg-white focus:outline-none  focus:ring-stone-400 font-medium rounded-full text-sm  me-2 mb-2 dark:bg-white dark:hover:border-stone-400 dark:hover:text-stone-400 dark:focus:ring-stone-400 dark:border-gray-800 px-[40px] py-[12px] border-1">Login</button></Link>
           </li>
           <li>
-            <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none  focus:ring-gray-300 font-medium rounded-full text-sm  me-2 mb-2 dark:bg-gray-800 dark:hover:bg-[hsla(36,4%,44%,1)] dark:focus:ring-gray-700 dark:border-stone-400 px-[40px] py-[12px] border-1 ">Signup</button>
+          <Link to="/register"><button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none  focus:ring-gray-300 font-medium rounded-full text-sm  me-2 mb-2 dark:bg-gray-800 dark:hover:bg-[hsla(36,4%,44%,1)] dark:focus:ring-gray-700 dark:border-stone-400 px-[40px] py-[12px] border-1 ">Signup</button></Link>
           </li>
         </ul>
       </nav>
